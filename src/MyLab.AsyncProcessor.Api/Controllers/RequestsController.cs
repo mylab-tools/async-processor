984667c38cb25d8a583779866b9bf602fb0a4052
Andrey O <ozzy@triasoft.com>
2020-11-03 01:38:12 +0300

    Define controller methods

--- src/MyLab.AsyncProcessor.Api/Controllers/RequestsController.cs
+++ src/MyLab.AsyncProcessor.Api/Controllers/RequestsController.cs
@@ -13,5 +13,51 @@ namespace MyLab.AsyncProcessor.Api.Controllers
         {
             return null;
         }
+
+        [HttpGet("{id}/status")]
+        public IAsyncResult GetStatus([FromRoute]string id)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}/status/biz-step")]
+        public IAsyncResult UpdateBizStep([FromRoute] string id, [FromBody]string bizStep)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}/status/error")]
+        public IAsyncResult SetError([FromRoute] string id, [FromBody] ProcessingError error)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}/result")]
+        [Consumes("application/json")]
+        public IAsyncResult SetResultJson([FromRoute] string id)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}/result")]
+        [Consumes("text/plain")]
+        public IAsyncResult SetResultText([FromRoute] string id)
+        {
+            return null;
+        }
+
+        [HttpPut("{id}/result")]
+        [Consumes("application/octet-stream")]
+        public IAsyncResult SetResultBin([FromRoute] string id)
+        {
+            return null;
+        }
+
+        [HttpGet("{id}/result")]
+        [Produces("application/octet-stream", "text/plain", "application/json")]
+        public IAsyncResult GetResult([FromRoute] string id)
+        {
+            return null;
+        }
     }
 }
